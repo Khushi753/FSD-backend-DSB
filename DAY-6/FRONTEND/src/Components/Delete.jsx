eb207c22aa01b36baed65064b3fcaf01864390cd
Khushi753 <khushisrivastav0808@gmail.com>
2025-04-03 22:37:34 +0530

    deploying

--- DAY-6/FRONTEND/src/Components/Delete.jsx
+++ DAY-6/FRONTEND/src/Components/Delete.jsx
@@ -5,7 +5,7 @@ const Delete = () => {
     const handledelete = async (e) => {
         e.preventDefault()
         const id = e.target.id.value
-        await axios.delete(`http://localhost:9000/users/${id}`)
+        await axios.delete(`https://fsd-backend-dsb-new.onrender.com/users/${id}`)
         alert('Product deleted successfully')
       }
 
